f914d2d3341304a761e0af767579c9e6e169351c
Ray Zane <ray@promptworks.com>
2017-06-12 23:21:20 -0400

    Go back to relying on glob

--- src/gsub/file_list.cr
+++ src/gsub/file_list.cr
@@ -1,10 +1,18 @@
+class Dir
+  # I'm a very, very naughty boy.
+  # See: https://github.com/crystal-lang/crystal/blob/5370fafdd8744c9f5b8443bc35c65fc6926d2341/src/dir/glob.cr#L108
+  def self._expand_glob(glob)
+    glob2regex(glob)
+  end
+end
+
 module Gsub
   class FileList
-    setter :includes, :excludes
+    setter :includes, :exclude_patterns
 
     def initialize
       @includes = [Dir.current] of String
-      @excludes = [] of String
+      @exclude_patterns = [] of Regex
     end
 
     def add(spec : String)
@@ -12,29 +20,36 @@ module Gsub
     end
 
     def remove(spec : String)
-      @excludes << spec
+      @exclude_patterns << Dir._expand_glob(spec)
     end
 
     def each(&block : (String) ->)
       @includes.each do |glob|
-        Process.run("find", find_args(glob)) do |process|
-          process.output.each_line(&block)
+        Dir.glob(glob) do |path|
+          next if excluded?(path)
+          next if binary?(path)
+          yield path
         end
       end
     end
 
-    private def find_args(glob)
-      [glob, "-type", "f"] + skip_excludes + skip_binaries
-    end
-
-    private def skip_excludes
-      @excludes.flat_map do |exclude|
-        ["-not", "(", "-path", exclude, "-prune", ")"]
+    def excluded?(path)
+      @exclude_patterns.any? do |exclude|
+        exclude =~ path
       end
     end
 
-    private def skip_binaries
-      ["-exec", "grep", "-Iq", ".", "{}", ";", "-and", "-print"]
+    # Returns true if the file includes a null byte
+    # in the first 4096 chars
+    def binary?(path)
+      file = File.new(path)
+      file.set_encoding("US-ASCII", invalid: :skip)
+
+      0.upto(4096).any? do
+        char = file.read_char
+        break false unless char
+        char == 0.chr
+      end
     end
   end
 end
